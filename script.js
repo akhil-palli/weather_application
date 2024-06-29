d5811da5dd0cc6ca78b56c0cd338a8676690332b
akhil-palli <akhildpalli@gmail.com>
2024-06-29 02:13:42 -0400

    Added icons and changed weather description elements.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const apiKey =
+const apiKey = ''
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 const locationInput = document.getElementById('locationInput');
 const searchButton = document.getElementById('searchButton');
-const locationElement = document.getElementById('location');
-const temperatureElement = document.getElementById('temperature');
-const descriptionElement = document.getElementById('description');
+const locationElement = document.querySelector('.weather-info .location');
+const descriptionElement = document.querySelector('.weather-info .description');
+const temperatureElement = document.querySelector('.weather-info .temperature');
 
 searchButton.addEventListener('click', () => {
     const location = locationInput.value;
@@ -23,34 +23,35 @@ function fetchWeather(location) {
             locationElement.textContent = data.name;
             temperatureElement.textContent = `${Math.round(data.main.temp)}°F`;
             descriptionElement.textContent = data.weather[0].description;
-            changeBackground();
+            changeBackgroundAndIcon();
+            
         })
         .catch(error => {
             console.error('Error fetching weather data:', error);
         });
 }
 
-function changeBackground(){
+
+function changeBackgroundAndIcon(){
     if(descriptionElement.textContent.includes("clear sky")) {
         document.body.style.backgroundImage = "url(https://i.pinimg.com/originals/3a/2a/8f/3a2a8f79d9d4a7d36a258fb129ba36f9.gif)";
         document.body.style.backgroundImage = "url('content/sun-pic.jpg')";
-
-    }   
+        document.getElementById('myImg').src = 'content/clear.png';
+    } 
     if(descriptionElement.textContent.includes("cloud")) {
         document.body.style.backgroundImage = "url(https://i.makeagif.com/media/8-08-2017/U8cAor.gif)";
         document.body.style.backgroundImage = "url('content/clouds-pic.jpg')";
+        document.getElementById('myImg').src = 'content/cloud.png';
     }
-    
-    if(descriptionElement.textContent.includes("light rain")) {
+    if(descriptionElement.textContent.includes("rain")) {
         document.body.style.backgroundImage = "url(https://i.makeagif.com/media/8-08-2017/U8cAor.gif)";
         document.body.style.backgroundImage = "url('content/raining-pic.jpg')";
+        document.getElementById('myImg').src = 'content/rain.png';
     }
     if(descriptionElement.textContent.includes("thunderstorms")) {
         document.body.style.backgroundImage = "url(https://i.makeagif.com/media/8-08-2017/U8cAor.gif)";
         document.body.style.backgroundImage = "url('content/thunderstorm-pic.jpg')";
+        document.getElementById('myImg').src = 'content/rain.png';
     }
-    
-    
-
-
 }
+
