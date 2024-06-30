7a04cfd09d27c756304542f49a85ab9b03c498b4
akhil-palli <akhildpalli@gmail.com>
2024-06-30 02:12:21 -0400

    Updated location not found, and cleaned up js and html file.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = ''
+const apiKey = 'aee13a7dbf7fbe7ea1b18af2e7c93aec';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 const locationInput = document.getElementById('locationInput');
@@ -7,25 +7,22 @@ const locationElement = document.querySelector('.weather-info .location');
 const descriptionElement = document.querySelector('.weather-info .description');
 const temperatureElement = document.querySelector('.weather-info .temperature');
 
-
 //click search button to search
-searchButton.addEventListener('click', () => {
-    const location = locationInput.value;
-    if (location) {
-        fetchWeather(location);
-    }
-});
+searchButton.addEventListener('click', fetchWeather);
 
 //enter to search
 locationInput.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
-        const location = locationInput.value;
-        if (location) {
-            fetchWeather(location);
+        fetchWeather();
     }
-      }});
+});
 
-function fetchWeather(location) {
+function fetchWeather() {
+    const location = locationInput.value;
+    if (!location) {
+        return;
+    }
+
     const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=imperial`;
 
     fetch(url)
@@ -35,36 +32,36 @@ function fetchWeather(location) {
             temperatureElement.textContent = `${Math.round(data.main.temp)}°F`;
             descriptionElement.textContent = data.weather[0].description;
             changeBackgroundAndIcon();
-            
+
         })
         .catch(error => {
             console.error('Error fetching weather data:', error);
             document.getElementById('myImg').src = 'content/LocationNotFound.png';
             locationElement.textContent = 'Location Not Found!';
-            temperatureElement.textContent = '   ';
-            descriptionElement.textContent  = '   ';
+            temperatureElement.textContent = '';
+            descriptionElement.textContent = '';
         });
 }
 
 //updates backgrounds and weather icons
-function changeBackgroundAndIcon(){
-    if(descriptionElement.textContent.includes("clear sky")) {
-        document.body.style.backgroundImage = "url(https://i.pinimg.com/originals/3a/2a/8f/3a2a8f79d9d4a7d36a258fb129ba36f9.gif)";
+function changeBackgroundAndIcon() {
+    if (descriptionElement.textContent.includes("clear sky")) {
+        // document.body.style.backgroundImage = "url(https://i.pinimg.com/originals/3a/2a/8f/3a2a8f79d9d4a7d36a258fb129ba36f9.gif)";
         document.body.style.backgroundImage = "url('content/sun-pic.jpg')";
         document.getElementById('myImg').src = 'content/clear.png';
-    } 
-    if(descriptionElement.textContent.includes("cloud")) {
-        document.body.style.backgroundImage = "url(https://i.makeagif.com/media/8-08-2017/U8cAor.gif)";
+    }
+    if (descriptionElement.textContent.includes("cloud")) {
+        // document.body.style.backgroundImage = "url(https://i.makeagif.com/media/8-08-2017/U8cAor.gif)";
         document.body.style.backgroundImage = "url('content/clouds-pic.jpg')";
         document.getElementById('myImg').src = 'content/cloud.png';
     }
-    if(descriptionElement.textContent.includes("rain")) {
-        document.body.style.backgroundImage = "url(https://i.makeagif.com/media/8-08-2017/U8cAor.gif)";
+    if (descriptionElement.textContent.includes("rain")) {
+        // document.body.style.backgroundImage = "url(https://i.makeagif.com/media/8-08-2017/U8cAor.gif)";
         document.body.style.backgroundImage = "url('content/raining-pic.jpg')";
         document.getElementById('myImg').src = 'content/rain.png';
     }
-    if(descriptionElement.textContent.includes("thunderstorms")) {
-        document.body.style.backgroundImage = "url(https://i.makeagif.com/media/8-08-2017/U8cAor.gif)";
+    if (descriptionElement.textContent.includes("thunderstorms")) {
+        // document.body.style.backgroundImage = "url(https://i.makeagif.com/media/8-08-2017/U8cAor.gif)";
         document.body.style.backgroundImage = "url('content/thunderstorm-pic.jpg')";
         document.getElementById('myImg').src = 'content/rain.png';
     }
